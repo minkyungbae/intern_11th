58017d7d0ca7c2b3bb6c6b03c3d2135117f40edc
minkyungbae <bmk0703@naver.com>
2025-04-19 02:09:50 +0900

    Urls: Add login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from accounts.views import signup_views, login_views
 app_name = "accounts"
 urlpatterns = [
     path("signup/", signup_views.SignupAPIView.as_view(), name="signup"), # 회원 가입
+    path("login/", login_views.LoginAPIView.as_view(), name="login"), # 로그인
 ]
