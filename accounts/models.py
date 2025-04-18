b26f97806dd7e6575429050b89d778409470fbd0
minkyungbae <bmk0703@naver.com>
2025-04-19 00:44:25 +0900

    Function: Add User model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # nickname 필드 추가
+    nickname = models.CharField(max_length=30)
+
+    # 객체의 문자열 표현 정리 : User view에서 쓸 때 username이 return 됨
+    def __str__(self):
+        return self.username
+
